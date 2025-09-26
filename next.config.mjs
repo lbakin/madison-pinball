4715710c68bb55cd60bedd6a7a7d1841c5ff8e8f
Lev Bakin <lev@onapproval.com>
2025-09-25 18:11:16 -0700

    fixing export in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+ 
   eslint: {
-    // Allow production builds to complete even if there are ESLint errors
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true
   },
-  // If you ever add TS and want to relax type errors too:
-  typescript: { ignoreBuildErrors: true },
+  
 };
-module.exports = nextConfig;
+
+export default nextConfig;
